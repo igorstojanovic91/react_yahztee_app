1207884c1740f3ed92a58fbfc6bfc6664d863855
igorstojanovic91 <i.stojanovic01@gmail.com>
2019-04-29 14:01:36 -0500

    Adding point descriptions

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import './RuleRow.css'
 
 class RuleRow extends Component {
-  constructor(props) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this)
-  }
-  handleClick () {
 
-  }
   render() {
+    const score = this.props.score
+    const disabled = score !== undefined
+    const description = this.props.description
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr className={`RuleRow RuleRow-${!disabled ? "active" : "disabled"}`}
+      onClick={!disabled ? this.props.doScore: null}>
         <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+        <td className="RuleRow-score">{!disabled ? description : this.props.score}</td>
       </tr>
     )
   }
